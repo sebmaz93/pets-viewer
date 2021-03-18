b0ec26db498c5d275b363c34a7b62c009e07f56b
sebmaz <titan9389@me.com>
2021-03-18 04:47:13 +0400

    change from swiftUI to UIKit

---
+++ pets-viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pets-viewer
+//
+//  Created by Sebouh Mazloumian on 18.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
