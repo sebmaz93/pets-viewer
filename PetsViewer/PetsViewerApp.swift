5d5ecf552cd5fea6912a8801e0a527fd92c49695
sebmaz <titan9389@me.com>
2021-03-18 04:13:24 +0400

    init commit

---
+++ PetsViewer/PetsViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsViewerApp.swift
+//  PetsViewer
+//
+//  Created by Sebouh Mazloumian on 18.03.21.
+//
+
+import SwiftUI
+
+@main
+struct PetsViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
